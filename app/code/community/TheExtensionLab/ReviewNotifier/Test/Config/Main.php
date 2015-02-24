45b338574e651a23bf4d2637899e7f8ca1eb93e7
James Anelay <jamesanelay@gmail.com>
2015-02-24 19:10:27 +0100

    Add Model class alias test

--- app/code/community/TheExtensionLab/ReviewNotifier/Test/Config/Main.php
+++ app/code/community/TheExtensionLab/ReviewNotifier/Test/Config/Main.php
@@ -1,5 +1,7 @@
 <?php class TheExtensionLab_ReviewNotifier_Test_Config_Main extends EcomDev_PHPUnit_Test_Case_Config
 {
-
-
+    public function testClassAiases()
+    {
+        $this->assertModelAlias('theextensionlab_reviewnotifier/observer','TheExtensionLab_ReviewNotifier_Model_Observer');
+    }
 }
